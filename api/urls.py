6c2bbc11255506154c1a19a432c64c0c933ec44d
Igor Markin <9588604@gmail.com>
2021-03-23 16:14:10 +0300

    configured jwt authentication

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from . import views
 
@@ -9,4 +11,6 @@ router_v1.register(r'users', views.UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
