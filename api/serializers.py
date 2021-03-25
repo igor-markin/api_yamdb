f97d32ca097cddbab62ef01ce04b7e19fd8d0dc4
Igor Markin <9588604@gmail.com>
2021-03-25 12:29:20 +0300

    added user serializer and permissions

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = ('first_name', 'last_name', 'username', 'bio', 'email', 'role')
+        read_only_fields = ('role', 'email')
+
+
+class AdminUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = ('first_name', 'last_name', 'username', 'bio', 'email', 'role')
