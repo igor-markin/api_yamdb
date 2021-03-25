b08f45764ac7e3e98644f8e675df5de3016f5e52
Igor Markin <9588604@gmail.com>
2021-03-25 12:07:55 +0300

    added roles and custom user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Roles(models.TextChoices):
+    USER = 'user', 'Пользователь'
+    MODERATOR = 'moderator', 'Модератор'
+    ADMIN = 'admin', 'Администратор'
+
+
+class User(AbstractUser):
+    bio = models.CharField('О себе', blank=True, null=True,)
+    email = models.EmailField('Email', unique=True)
+    role = models.CharField('Роль', choices=Roles.choices, default=Roles.USER)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    class Meta:
+        ordering = ('username',)
